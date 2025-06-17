32c762635ab8c7edc80c0bd1e8c149bbdc426593
gagem <gagemann2007@gmail.com>
2025-06-16 23:52:01 -0400

    Commit 2, added Steel Ingot

--- src/main/java/net/chronic/additionstwo/AdditionsTwo.java
+++ src/main/java/net/chronic/additionstwo/AdditionsTwo.java
@@ -1,5 +1,6 @@
 package net.chronic.additionstwo;
 
+import net.chronic.additionstwo.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class AdditionsTwo implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
